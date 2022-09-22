0f07c60afe3a4f615e9718b599c6d0141ee155c1
ainsleysm <smithainsley21@gmail.com>
2022-09-22 18:45:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+       String party = "P";
+
+       if(party == "D") {
+           System.out.println("You get a Democratic Donkey.");
+       } else if(party == "R") {
+           System.out.println("You get a Republican Elephant.");
+       } else if(party == "I") {
+           System.out.println("You get an Independent Person.");
+       } else {
+           System.out.println("Other.");
+       }
+    }
+}
